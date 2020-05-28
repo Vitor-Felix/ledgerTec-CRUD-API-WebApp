2347c3c36db32333abd6bf33bfa5231c37dcd57d
Vitor-Felix <vytorfelix@gmail.com>
2020-05-28 06:45:31 -0300

    BD com dados do seed e novos seeds para produto

--- backend/database/seeders/20200521040423-Produto.js
+++ backend/database/seeders/20200521040423-Produto.js
@@ -20,6 +20,24 @@ module.exports = {
         "Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
       createdAt: new Date(),
       updatedAt: new Date()
+    }, {
+      categoriaId: 4,
+      descricao: 
+        "Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      categoriaId: 3,
+      descricao: 
+        "Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      categoriaId: 2,
+      descricao: 
+        "Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {});
 },
 
